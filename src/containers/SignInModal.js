8ad7ad9cabaf1c2f40e7327d03671eb44ef1c351
Emika Ijuin <emikaijuin@gmail.com>
2019-04-08 11:21:47 +0800

    User can sign in
    - Add event listeners for input change and form submit
    - Extract user sign in logic to helper

--- src/containers/SignInModal.js
+++ src/containers/SignInModal.js
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
-import { Dialog, Button } from "@material-ui/core";
+import { Dialog, Input } from "@material-ui/core";
+import UserAuth from "../helpers/UserAuth";
 
 class SignInModal extends Component {
+  state = {
+    user: {
+      email: "",
+      password: ""
+    }
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    UserAuth.signIn(this.state.user);
+    this.props.userSignedIn();
+  };
+
+  handleInputChange = event => {
+    const userState = { ...this.state.user };
+    userState[event.target.name] = event.target.value;
+    this.setState({ user: { ...userState } });
+  };
+
   render() {
     return (
       <Dialog open={this.props.isOpen} onClose={this.props.handleClose}>
-        <input />
-        <Button>Click me!</Button>
+        <form onSubmit={this.handleFormSubmit}>
+          <div>
+            <Input
+              placeholder="email"
+              name="email"
+              onChange={this.handleInputChange}
+              value={this.state.user.email}
+            />
+          </div>
+          <div>
+            <Input
+              placeholder="password"
+              type="password"
+              name="password"
+              onChange={this.handleInputChange}
+              value={this.state.user.password}
+            />
+          </div>
+          <div>
+            <Input type="submit">Sign In</Input>
+          </div>
+        </form>
       </Dialog>
     );
   }
