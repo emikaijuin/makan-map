c4cbbb878f5d372127399db0adfc02649290cd70
Emika Ijuin <emikaijuin@gmail.com>
2019-04-08 11:20:12 +0800

    Finish User Auth portions of Nav (no styling)
    - If user is logged out, show sign in and sign up buttons
    - If user is logged in, show log out button
    - Close modals after user signs in
    - Update state after user signs in or out

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -6,30 +6,50 @@ import SignUpModal from "./SignUpModal";
 class Nav extends Component {
   state = {
     showSignUp: false,
-    showSignIn: false
+    showSignIn: false,
+    userSignedIn: !!localStorage.getItem("auth_token")
   };
 
   showSignIn = () => this.setState({ showSignIn: true });
 
-  showSignUp = () => this.setState({ showSignUp: true });
-
   closeSignIn = () => this.setState({ showSignIn: false });
 
+  showSignUp = () => this.setState({ showSignUp: true });
+
   closeSignUp = () => this.setState({ showSignUp: false });
+
+  userSignedIn = () => {
+    this.setState({ userSignedIn: true });
+    this.closeSignIn();
+    this.closeSignUp();
+  };
+
+  userSignedOut = () => {
+    localStorage.removeItem("auth_token");
+    this.setState({ userSignedIn: false });
+  };
 
   render() {
     return (
       <AppBar>
-        <Button onClick={this.showSignIn}>Log In</Button>
-        <Button onClick={this.showSignUp}>Sign Up</Button>
+        {this.state.userSignedIn ? (
+          <Button onClick={this.userSignedOut}>Log Out</Button>
+        ) : (
+          <div>
+            <Button onClick={this.showSignIn}>Log In</Button>
+            <Button onClick={this.showSignUp}>Sign Up</Button>
+          </div>
+        )}
 
         <SignUpModal
           isOpen={this.state.showSignUp}
           handleClose={this.closeSignUp}
+          userSignedIn={this.userSignedIn}
         />
         <SignInModal
           isOpen={this.state.showSignIn}
           handleClose={this.closeSignIn}
+          userSignedIn={this.userSignedIn}
         />
       </AppBar>
     );
