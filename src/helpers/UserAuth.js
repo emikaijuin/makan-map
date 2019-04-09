74e6ba34d347a6a0fb0d945d0530f54541d6785e
Emika Ijuin <emikaijuin@gmail.com>
2019-04-09 11:22:17 +0800

    accept function as second parameter

--- src/helpers/UserAuth.js
+++ src/helpers/UserAuth.js
@@ -1,15 +1,17 @@
 import axios from "axios";
+import humps from "humps";
 
 export default {
-  signIn: user => {
+  signIn: (user, userSignedIn) => {
     return axios
       .post("http://localhost:3001/api/v1/user_token", {
         auth: {
-          ...user
+          ...humps.decamelizeKeys(user)
         }
       })
       .then(result => {
         localStorage.setItem("auth_token", result.data.jwt);
+        userSignedIn();
       })
       .catch(error => {
         console.log(error);
