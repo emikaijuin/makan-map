6177e072b23e2fd7fadddcb1d5b0e5fed7863a40
Emika Ijuin <emikaijuin@gmail.com>
2019-04-11 12:51:59 +0800

    pass sign in state to nav and only render mpa if user is signed in'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
 import Nav from "./containers/Nav";
+import MapPage from "./pages/MapPage";
 
 class App extends Component {
+  state = {
+    userIsSignedIn: !!localStorage.getItem("auth_token")
+  };
+
+  userSignedIn = () => {
+    this.setState({ userSignedIn: true });
+  };
+
+  userSignedOut = () => {
+    localStorage.removeItem("auth_token");
+    this.setState({ userSignedIn: false });
+  };
+
   render() {
     return (
       <div className="App">
-        <Nav />
-        <header className="App-header">Makan Map</header>
+        <Nav
+          userIsSignedIn={this.state.userIsSignedIn}
+          userSignedIn={this.userSignedIn}
+          userSignedOut={this.userSignedOut}
+        />
+        {this.state.userIsSignedIn ? (
+          <MapPage />
+        ) : (
+          <header className="App-header">Makan Map</header>
+        )}
       </div>
     );
   }
