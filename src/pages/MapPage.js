d2d9b61125f35ded4702c514ca41dc96fecdeb29
Emika Ijuin <emikaijuin@gmail.com>
2019-04-12 17:58:05 +0800

    WIP - Set map center and coordinates
    - On map load
    -- Retrieve user coordinates using browser geolocation and center map on coordinates
    -- Send boundaries of map view to server and retrieve all listings which fall within the boundaries
    
    - On map change
    -- Send boundaries of map view to server and retrieve all listings which fall within the boundaries

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
+import { PinDrop } from "@material-ui/icons";
+import axios from "axios";
 
 class MapPage extends Component {
-  state = {};
+  state = {
+    center: {
+      lat: "42.3564",
+      long: "-71.0617"
+    },
+    currentBounds: {}
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(result => {
+      const coords = {
+        lat: result.coords.latitude,
+        long: result.coords.longitude
+      };
+      this.updateCenterCoords(coords);
+    });
+  }
+
+  handleMapChange = result => {
+    this.updateBoundaryCoords(result);
+    this.retrieveMarkers();
+  };
+
+  retrieveMarkers = () => {
+    axios
+      .get("http://localhost:3001/api/v1/listings", {
+        params: { bounds: { ...this.state.currentBounds } }
+      })
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  updateCenterCoords = coords => {
+    this.setState({ center: coords });
+  };
+
+  updateBoundaryCoords = result => {
+    this.setState({ currentBounds: result.bounds });
+  };
 
   render() {
     return (
@@ -11,7 +52,11 @@ class MapPage extends Component {
         bootstrapURLKeys={{ key: "AIzaSyBUDWlXGgvD6lw5j-afntTLx5cdRGmV_3c" }}
         defaultCenter={{ lat: 59.95, lng: 30.33 }}
         zoom={11}
-      />
+        onGoogleApiLoaded={this.handleMapChange}
+        onChange={this.handleMapChange}
+      >
+        <PinDrop lat={42.3564} lng={-71.0617} />
+      </GoogleMapReact>
     );
   }
 }
